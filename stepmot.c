e78984ce85a3e5d4c8d4f23987c46dee75c37fbb
Aniketh Hebbar <76802363+anikethhebbar@users.noreply.github.com>
2022-09-10 07:20:48 +0530

    steppermotor

---
+++ stepmot.c
@@ -0,0 +1,34 @@
+#include<lpc214x.h>
+unsigned long int var=0;
+unsigned int i=0,j=0,k=0;
+int main(void)
+{
+    PINSEL0=0X00000000;
+    IO0DIR=OXOOOOF0000;
+    while(1)
+    {
+        for(j=0;j<50;j++)
+        {
+            var=0x0001000;//clockwise
+            for(i=0;i<=3;i++)
+            {
+                IO0PIN=var;
+                for(k=0;k<50000;k++);
+                var=var<<1;
+            }//clockwise ends
+        }for(k=0;k<60000;k++);//delay
+
+        for(j=0;j<50;j++)
+        {
+            var=0x00008000;//anticlockwise
+            for(i=0;i<=3;i++)
+            {
+                IO0PIN=var;
+                for(k=0;k<50000;k++);
+                var=var>>1;
+            }//anticlockwise ends
+        }for(k=0;k<60000;k++);//delay
+
+
+    }
+}
